7fe920ca8c3fc963728dff3d15bff3dd10501485
michaelpappas <pappas.michael@gmail.com>
2023-05-22 16:18:22 -0700

    create waterMQTT script to send water usage to broker

--- flowCalib.py
+++ flowCalib.py
@@ -1,5 +1,21 @@
 import machine
 import utime
+import network
+import time
+from umqtt.simple import MQTTClient
+
+# Configure network connection
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(SSID,WIFI_PASSWORD)
+time.sleep(5)
+print(wlan.isconnected())
+
+# Configure MQTT
+mqtt_server = "192.168.1.86"
+client_id = 'bigles'
+topic_pub = b'water'
+topic_msg = b'Movement Detected'
 
 # Configure GPIO pins
 flow_pin = machine.Pin(15, machine.Pin.IN)
@@ -18,6 +34,24 @@ def handle_interrupt(pin):
 # Configure interrupt on falling edge of flow sensor signal
 flow_pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=handle_interrupt)
 
+
+# Connect to MQTT client
+def mqtt_connect():
+    client = MQTTClient(client_id, mqtt_server, keepalive=3600)
+    client.connect()
+    print('Connected to %s MQTT Broker'%(mqtt_server))
+    return client
+
+def reconnect():
+    print('Failed to connect to the MQTT Broker. Reconnecting...')
+    time.sleep(5)
+    machine.reset()
+
+try:
+    client = mqtt_connect()
+except OSError as e:
+    reconnect()
+
 # Main loop to calculate and print total water flow
 while True:
     # Calculate liters per minute based on flow frequency
@@ -32,6 +66,8 @@ while True:
     # Print total liters and reset flow frequency
     print("Total water flow: {:.2f} liters".format(total_liters))
     flow_frequency = 0
-
-    # Delay for 1 second
-    utime.sleep(1)
+    message = f"{total_liters}".encode()
+    # Delay for 5 second
+    utime.sleep(5)
+    # Send total_liters to MQTT Broker
+    client.publish(topic_pub, message)
